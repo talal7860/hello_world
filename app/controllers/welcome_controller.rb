43fb71e8a7276c330f60159c7019108913047761
Talal Arshad <talal7860@gmail.com>
2018-03-23 17:30:52 +0500

    hello world 10

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     render json: {
-      text: 'Hello World 9',
+      text: 'Hello World 10',
       ip: Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address)
     }
   end

5e81d2ea28be8bbb0a72be51fad6887d50976c08
Talal Arshad <talal7860@gmail.com>
2018-04-02 22:02:13 +0500

    rails master key

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
   def index
     render json: {
       text: 'Hello World 10',
-      ip: Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address)
+      ip: Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address),
+      'RAILS_MASTER_KEY': ENV.fetch('RAILS_MASTER_KEY')
     }
   end
 end

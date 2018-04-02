92232536fb3f87c557c1f9e2bacfbed7a72a7713
Talal Arshad <talal7860@gmail.com>
2018-04-02 22:05:52 +0500

    some linting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     render json: {
       text: 'Hello World 10',
       ip: Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address),
-      'RAILS_MASTER_KEY': ENV.fetch('RAILS_MASTER_KEY')
+      RAILS_MASTER_KEY: ENV.fetch('RAILS_MASTER_KEY')
     }
   end
 end

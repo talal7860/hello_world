c80a08052b8c778603a9a75ead034bf612f91df9
Talal Arshad <talal7860@gmail.com>
2018-04-10 16:01:42 +0500

    updating hello world

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     render json: {
-      text: 'Hello World 10',
+      text: 'Hello World 11',
       ip: Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address),
       RAILS_MASTER_KEY: ENV.fetch('RAILS_MASTER_KEY')
     }
